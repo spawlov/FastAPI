a2e57c89b8cab9209cb202b0d472cff41026b633
sergey <spawlov@gmail.com>
2024-08-25 16:23:31 +0800

    Add two lessons

---
+++ 3.1.7/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Item:
+    return item

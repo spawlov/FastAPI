15ed3632a4a88f81d7207e47e05b53e096370049
sergey <spawlov@gmail.com>
2024-08-30 11:44:06 +0800

    Added lesson 6.1.7

---
+++ 6.1.7/app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+
+class CustomException(HTTPException):
+    def __init__(self, detail: str, status_code: int = 400):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+@app.exception_handler(CustomException)
+async def custom_exception_handler(
+    request: Request, exc: CustomException
+) -> JSONResponse:
+    return JSONResponse(status_code=exc.status_code, content={"error": exc.detail})
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(reuest: Request, exc: Exception) -> JSONResponse:
+    return JSONResponse(status_code=500, content={"error": "Internal server error"})
+
+
+@app.get("/items/{item_id}/")
+async def read_item(item_id: int) -> dict[str, int]:
+    if item_id == 42:
+        raise CustomException(status_code=404, detail="Item not found")
+    result = 1 / 0
+    return {"item_id": item_id}
